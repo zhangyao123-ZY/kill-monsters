7b4c7f14589550d6c89b69c6141f2fee3de8c288
zhang <1824715008@qq.com>
2025-11-11 10:24:59 +0800

    fix: 去除dist上传

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss(),],
-  base: process.env.NODE_ENV === 'production' ? '/kill-monsters/' : '/'
+  base: process.env.NODE_ENV === 'production' ? '/kill-monsters/' : '/',
+  build: {
+    outDir: 'docs',
+  }
 })
